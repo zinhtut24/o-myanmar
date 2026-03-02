26fdfbb37af40034e213fb8e389148ce5967cb4b
Zin Htut Naing <zinhtutnaing2410@gmail.com>
2026-03-02 16:15:12 +0630

    Fix: add prisma generate to build script

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,3 +13,4 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
