ed3c10d754350d6910601b1fe337fb5cf97d6930
Zin Htut Naing <zinhtutnaing2410@gmail.com>
2026-03-02 16:31:48 +0630

    First commit - O-Myanmar Travel Website

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-// Prisma 7 အတွက် Database Adapter ချိတ်ဆက်ခြင်း
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-// Next.js မှာ Reload ခဏခဏလုပ်ရင် Database ကြောင်မသွားအောင် ကာကွယ်ပေးတဲ့စနစ်
+// Next.js မှာ Hot Reload ဖြစ်ရင် Connection တွေ အများကြီး ထပ်မပွားအောင် ကာကွယ်ပေးတဲ့စနစ်
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
