c038653947db5ad0ff248c9ff731db1e30117fb1
Zin Htut Naing <zinhtutnaing2410@gmail.com>
2026-03-07 22:29:31 +0630

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,32 +3,30 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // အားလုံးသော Routes တွေအတွက် သတ်မှတ်မည်
         source: '/(.*)',
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'SAMEORIGIN', // Clickjacking ကာကွယ်ရန်
+            value: 'SAMEORIGIN',
           },
           {
             key: 'X-Content-Type-Options',
-            value: 'nosniff', // MIME-sniffing ကာကွယ်ရန်
+            value: 'nosniff', 
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin', // Referrer data ထိန်းချုပ်ရန်
+            value: 'origin-when-cross-origin', 
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()', // မလိုအပ်သော Browser Features များ ပိတ်ရန်
+            value: 'camera=(), microphone=(), geolocation=()', 
           },
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains; preload', // HTTPS သာ သုံးရန် အတင်းအကျပ် ခိုင်းစေခြင်း
+            value: 'max-age=31536000; includeSubDomains; preload', 
           },
           {
             key: 'Content-Security-Policy',
-            // အစ်ကို့ Website မှာ သုံးထားတဲ့ Image/Script Domains တွေကို ဒီမှာ Whitelist လုပ်ပေးရပါမယ်
             value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: images.unsplash.com res.cloudinary.com; connect-src 'self'; font-src 'self' data:;",
           },
         ],
